b607fc12d7beaa26db73c420c5e542270106bae9
Francis <franciswanjagi0@gmail.com>
2024-06-11 14:06:21 +0300

    added authentication on sign up page with firebase

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,21 +1,61 @@
 import React from "react";
 import "./signup.css"
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 const Signup = () => {
+
+    //create the states for username emmail and password
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    // create function to handle register
+    const handleRegister = async (e)=>{
+        e.preventDefault();
+
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            const user = auth.currentUser;
+            console.log(user);
+            console.log("User Registered Successfully")
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return (
         <div className="wrapper">
             <div className="form-box">
-                <form action="">
+                <form action="" onSubmit={handleRegister}>
                     <h1>Register</h1>
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required/>
+                        <input
+                         type="text" 
+                         placeholder="Username"
+                         value={username}
+                         onChange={(e) => setUsername(e.target.value)}
+                         required
+                        />
                     </div>
                     <div className="input-box">
-                        <input type="email" placeholder="Email" required/>
+                        <input
+                         type="email" 
+                         placeholder="Email"
+                         value={email}
+                         onChange={(e) => setEmail(e.target.value)}
+                         required
+                        />
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Password" required/>
+                        <input
+                         type="password" 
+                         placeholder="Password" 
+                         value={password}
+                         onChange={(e) => setPassword(e.target.value)}
+                         required
+                        />
                     </div>
                     <div className="remember">
                         <label><input type="checkbox" />I agree to the terms and conditions</label>
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxONynp3bOvFfKurXjTtgwpeujwqSHzDk",
+  authDomain: "login-auth-8f2ec.firebaseapp.com",
+  projectId: "login-auth-8f2ec",
+  storageBucket: "login-auth-8f2ec.appspot.com",
+  messagingSenderId: "667648881437",
+  appId: "1:667648881437:web:e716bd1a57037a1cf667f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// export getAuth to help register users
+export const auth=getAuth();
+export default app;
